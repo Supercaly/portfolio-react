8bf6b4ba34680d72f95a46bab9181d072b74165c
Lorenzo Calisti <djonline96@gmail.com>
2022-12-18 12:03:18 +0100

    Fix `Badge`'s alignment error

--- src/components/widgets/Badge.js
+++ src/components/widgets/Badge.js
@@ -2,16 +2,17 @@ import React from "react";
 import styles from './Badge.module.css';
 
 function Badge(props) {
-    return (<div className={styles.box}>
-        <h6 className={styles.text}>{formatDate(props.text)}</h6>
-    </div>);
-}
-
-function formatDate(dates) {
-    if (Array.isArray(dates)) {
-        return dates.join(' - ');
+    if (!Array.isArray(props.text)) {
+        throw Error('badge dates must be an array of dates')
     }
-    return dates;
+    if (props.text.length == 0 || props.text.length > 2) {
+        throw Error('badge dates array must have one or two elements')
+    }
+    let dates_txt = props.text.join(' - ')
+    let margin = props.text.length == 1 ? 60: 0
+    return (<div className={styles.box} style={{marginRight: margin+'px'}}>
+        <h6 className={styles.text}>{dates_txt}</h6>
+    </div>);
 }
 
 export default Badge;
